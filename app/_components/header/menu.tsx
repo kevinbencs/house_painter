a466c6bf1131c9f1831724a923c6eb8b00322a46
Kevin Bencs <kevinbencs8@gmail.com>
2026-03-31 23:51:45 +0200

    Cahnged the color and the animation on hamburger menu

--- app/_components/header/menu.tsx
+++ app/_components/header/menu.tsx
@@ -1,7 +1,8 @@
 "use client"
-import { useState, useEffect } from "react"
+
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react"
 
 
 const Menu = () => {
@@ -23,21 +24,21 @@ const Menu = () => {
         <div className="flex justify-between md:justify-around">
             <Link href="/">Image</Link>
             <div >
-                <Button variant="outline" className="lg:hidden text-center text-black" onClick={() => { setIsOpen(true) }}>
-                    <label htmlFor="hamburger-menu">Menu</label>
-                </Button>
-                <input type="checkbox" name="hamburger-menu" className="hidden" id="hamburger-menu" />
+                <label htmlFor="hamburger-menu" className="lg:hidden text-center text-black p-2 rounded bg-gray-50">Menu</label>
+
+                <input type="checkbox" name="hamburger-menu" className="hidden" id="hamburger-menu" onChange={() => setIsOpen(!isOpen)}/>
                 <div className='fixed z-50 duration-100 inset-0 -translate-y-full lg:translate-y-0 lg:static'>
-                    <div className="h-screen w-full bg-gray-900 p-2 z-20 lg:z-0 lg:h-auto lg:static lg:w-auto">
-                        <Button variant="outline" className="text-black lg:hidden" onClick={() => setIsOpen(false)}><label htmlFor="hamburger-menu">X</label></Button>
-                        <section className=" h-full justify-around  flex flex-col lg:static lg:flex-row lg:justify-between lg:gap-4">
-                            <Button variant="link" className="-translate-y-full relative duration-300 lg:translate-y-0 delay-500"><Link className="text-white " href="/">Főoldal</Link></Button>
-                            <Button variant="link" className="-translate-y-full relative duration-300 lg:translate-y-0 delay-600"><Link className="text-white" href="/kepek">Képek</Link></Button>
-                            <Button variant="link" className="-translate-y-full relative duration-300 lg:translate-y-0 delay-700"><Link className="text-white" href="/arak">Árak</Link></Button>
-                            <Button variant="link" className="-translate-y-full relative duration-300 lg:translate-y-0 delay-800"><Link className="text-white" href="/kapcsolat">Kapcsolat</Link></Button>
-                            <Button variant="link" className="-translate-y-full relative duration-300 lg:translate-y-0 delay-900"><Link className="text-white" href="/blog">Blogok</Link></Button>
-                            <Button variant="link" className="-translate-y-full relative duration-300 lg:translate-y-0 delay-1000"><Link className="text-white" href="/szolgaltatas">Szolgáltatások</Link></Button>
-                            <Button variant="link" className="-translate-y-full relative duration-300 lg:translate-y-0 delay-1100"><Link className="text-white" href="/dashboard">Dashboard</Link></Button>
+                    <div className="h-screen w-full bg-black p-2 z-20 lg:z-0 lg:h-auto lg:bg-gray-900 lg:static lg:w-auto">
+                        <label htmlFor="hamburger-menu" className="text-black lg:hidden p-2 rounded bg-gray-50">X</label>
+                        <section className=" h-full justify-around  flex flex-col lg:static lg:flex-row lg:justify-between lg:gap-4 items-center">
+                            <Button variant="link" className=" ease-in-out -top-full relative duration-300 lg:static delay-800"><Link className="text-white " href="/">Főoldal</Link></Button>
+                            <Button variant="link" className=" ease-in-out -top-full relative duration-300 lg:static delay-700"><Link className="text-white" href="/kepek">Képek</Link></Button>
+                            <Button variant="link" className=" ease-in-out -top-full relative duration-300 lg:static delay-600"><Link className="text-white" href="/arak">Árak</Link></Button>
+                            <Button variant="link" className=" ease-in-out -top-full relative duration-300 lg:static delay-500"><Link className="text-white" href="/kapcsolat">Kapcsolat</Link></Button>
+                            <Button variant="link" className=" ease-in-out -top-full relative duration-300 lg:static delay-400"><Link className="text-white" href="/blog">Blogok</Link></Button>
+                            <Button variant="link" className=" ease-in-out -top-full relative duration-300 lg:static delay-300"><Link className="text-white" href="/szolgaltatas">Szolgáltatások</Link></Button>
+                            <Button variant="link" className=" ease-in-out -top-full relative duration-300 lg:static delay-200"><Link className="text-white" href="/dashboard">Dashboard</Link></Button>
+                            <div className=" ease-in-out -top-full relative duration-300 lg:static delay-100">Time</div>
                         </section>
                     </div>
                 </div>
