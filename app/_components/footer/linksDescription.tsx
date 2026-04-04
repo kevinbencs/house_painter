66ce49017b895da4e7b714ac95d3196f396d1a82
Kevin Bencs <kevinbencs8@gmail.com>
2026-04-04 23:37:01 +0200

    Practised the animation error in the menu. Social media icons are in the footer.

--- app/_components/footer/linksDescription.tsx
+++ app/_components/footer/linksDescription.tsx
@@ -1,23 +1,28 @@
 import Link from "next/link"
+import SocialMeadia from "./socialMedia"
 
 
 const LinksDescription = () => {
   return (
-    <div className="max-w-[1000px]  flex  flex-col flex-wrap gap-10 md:gap-x-40  content-center md:flex-row  md:justify-between">
+    <div className="max-w-[1000px] text-sm    flex  flex-col flex-wrap gap-10 md:gap-x-40  content-center md:flex-row  md:justify-between">
       <section className="flex flex-col ">
-        <Link href="/impresszium">Impresszium</Link>
-        <Link href="/sutik">Sütik</Link>
-        <Link href="/kerdesek">GYIK</Link>
-        <Link href="/adatvedelem">Adatvédelmi tájékoztató</Link>
+        <Link href="/impresszium" className="hover:underline">Impresszium</Link>
+        <Link href="/sutik" className="hover:underline">Sütik</Link>
+        <Link href="/kerdesek" className="hover:underline">GYIK</Link>
+        <Link href="/adatvedelem" className="hover:underline">Adatvédelmi tájékoztató</Link>
       </section>
       <article>
-        <h3><Link href='/helyek'>Ahol jelen vagyunk</Link></h3>
+        <h3 className="mb-3"><Link href='/helyek' className="hover:underline">Ahol jelen vagyok</Link></h3>
         <section>
 
         </section>
       </article>
       <article>
-        <h3><Link href="/szolgaltatas">Szolgáltatásaim</Link></h3>
+        <h3 className="mb-3"><Link href="/szolgaltatas" className="hover:underline">Szolgáltatásaim</Link></h3>
+      </article>
+      <article>
+        <h3 className="mb-3">Egyéb felületek</h3>
+        <SocialMeadia/>
       </article>
     </div>
   )
--- app/_components/header/menu.tsx
+++ app/_components/header/menu.tsx
@@ -23,25 +23,25 @@ const Menu = () => {
     }, [isOpen])
 
     return (
-        <div className="flex justify-between md:justify-around items-center">
+        <div className="flex justify-between lg:justify-around items-center">
             <Link href="/">Image</Link>
             <div >
                 <label htmlFor="hamburger-menu" className="lg:hidden text-center text-black p-2 rounded bg-gray-50">Menu</label>
 
-                <input type="checkbox" name="hamburger-menu" className="hidden" id="hamburger-menu" onChange={() => setIsOpen(!isOpen)} />
+                <input type="checkbox" name="hamburger-menu" className="fixed -right-64 lg:hidden" id="hamburger-menu" onChange={() => setIsOpen(!isOpen)} />
                 <div className='fixed z-50 duration-100 inset-0 -translate-y-full lg:translate-y-0 lg:static'>
                     <div className="h-screen w-full bg-black p-2 z-20 lg:z-0 lg:h-auto lg:bg-gray-900 lg:static lg:w-auto">
                         <label htmlFor="hamburger-menu" className="text-black lg:hidden p-2 rounded bg-gray-50">X</label>
                         <section className=" h-full justify-around  flex flex-col lg:static lg:flex-row lg:justify-between lg:gap-4 items-center">
-                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-800 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/">Főoldal</Link>
-                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-800 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/kepek">Képek</Link>
-                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-800 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/arak">Árak</Link>
-                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-800 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/kapcsolat">Kapcsolat</Link>
-                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-800 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/blog">Blogok</Link>
-                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-800 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/szolgaltatas">Szolgáltatások</Link>
+                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-700 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/">Főoldal</Link>
+                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-600 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/kepek">Képek</Link>
+                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-500 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/arak">Árak</Link>
+                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-400 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/kapcsolat">Kapcsolat</Link>
+                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-300 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/blog">Blogok</Link>
+                            <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-200 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/szolgaltatas">Szolgáltatások</Link>
                             {IsLogged &&
                                 <>
-                                    <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-800 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/dashboard">Dashboard</Link>
+                                    <Link className="text-white ease-in-out -top-full relative duration-300 lg:static delay-200 p-px pl-[11px] text-sm font-medium mt-1 mb-1 pr-[11px] hover:underline" href="/dashboard">Dashboard</Link>
                                     <div className=" ease-in-out -top-full relative duration-300 lg:static delay-100">{Math.floor(Timer/60)}:{String(Timer%60).padStart(2, '0')}</div>
                                 </>
                             }
