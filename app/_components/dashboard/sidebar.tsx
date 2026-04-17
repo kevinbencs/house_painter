6356db2918b4b5759c5a478f85402172862a4e59
Kevin Bencs <kevinbencs8@gmail.com>
2026-04-17 17:42:17 +0200

    Add 3 link to the dashboard

--- app/_components/dashboard/sidebar.tsx
+++ app/_components/dashboard/sidebar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import LogoutButton from "./logoutButton"
 import { v4 as uuidv4 } from "uuid"
 import { usePathname } from "next/navigation"
-import { FaPencil } from "react-icons/fa6";
+import { FaBloggerB, FaCity, FaPencil } from "react-icons/fa6";
 import { FaWallet } from "react-icons/fa6";
 import { MdDashboard } from "react-icons/md";
 import { FaLock } from "react-icons/fa";
@@ -13,6 +13,8 @@ import { FaFileImage } from "react-icons/fa";
 import { IoIosImages } from "react-icons/io";
 import { FaMap } from "react-icons/fa";
 import { FaPaintRoller } from "react-icons/fa";
+import { LuPaintBucket } from "react-icons/lu"
+
 
 const Links = [
   { url: "/dashboard", text: 'Dashboard', img: <MdDashboard /> },
@@ -21,16 +23,19 @@ const Links = [
   { url: "/dashboard/new/image", text: 'Új Kép hozzáadása', img: <FaFileImage /> },
   { url: "/dashboard/price", text: 'Árak', img: <FaWallet /> },
   { url: "/dashboard/new/blog", text: 'Új blog', img: <FaPencil /> },
+  { url: "/dashboard/update/blog", text: 'Blogok', img: <FaBloggerB /> },
   { url: "/dashboard/update/image", text: 'Képek szerkesztése', img: <IoIosImages /> },
   { url: "/dashboard/new/service", text: 'Új szolgáltatás írása', img: <FaPaintRoller /> },
+  { url: "/dashboard/update/service", text: 'Szolgáltatások', img: <LuPaintBucket /> },
   { url: "/dashboard/new/place", text: 'Új hely hozzáadása', img: <FaMap /> },
+  { url: "/dashboard/update/service", text: 'Helyek', img: <FaCity /> },
 ]
 
 const Sidebar = () => {
   const pathname = usePathname()
   return (
     <div >
-      <section className="flex sticky top-0 h-[700px] flex-col items-start justify-between bg-gray-900 p-10 text-gray-400 rounded pb-4 text-sm min-w-[251px]">
+      <section className="flex sticky top-0 h-[800px] flex-col items-start justify-between bg-gray-900 p-10 text-gray-400 rounded pb-4 text-sm min-w-[251px]">
         {Links.map((item) => <Link className={`hover:text-gray-200  flex items-center gap-2 w-full p-2 rounded-xs ${pathname === item.url ? 'text-white' : ''}`} key={uuidv4()} href={item.url}>{item.img} {item.text}</Link>)}
         <LogoutButton />
       </section>
