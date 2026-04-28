c77553f462079c9413ebf796813f37928ee0d6bd
Kevin Bencs <kevinbencs8@gmail.com>
2026-04-28 22:07:10 +0200

    Worked on the UI of delete image.

--- app/_components/dashboard/updateImage/imageUpdaetPage.tsx
+++ app/_components/dashboard/updateImage/imageUpdaetPage.tsx
@@ -17,18 +17,22 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { updateImage } from "@/action/updateImage";
 import { Checkbox } from "@/components/ui/checkbox"
+import { deleteImage } from "@/action/deleteImage";
+import { error } from "console";
 
 
 interface Img {
     _id: string,
     newUrl: string,
     detail: string,
-    show:boolean
+    show: boolean
 }
 
 const ImageUpdaetPage = (props: { img: Img[] }) => {
     const [lightBox, setLightBox] = useState<Img>({ newUrl: "", detail: '', _id: "", show: true })
     const [state, action, isPending] = useActionState(updateImage, null)
+    const [err, setErr] = useState<string>('')
+    const [mess, setMess] = useState<string>('')
 
     const clickOnImage = async (newUrl: string, detail: string, _id: string, show: boolean) => {
         setLightBox({ newUrl, detail, _id, show });
@@ -52,8 +56,46 @@ const ImageUpdaetPage = (props: { img: Img[] }) => {
     }
 
 
+    const deleteImg = async () => {
+        try {
+            const res = await deleteImage(lightBox._id)
+
+            if (res.error) {
+                console.log(error);
+                setErr("Hiba: " + res.error)
+                setTimeout(() => {
+                    setErr('')
+                }, 5000)
+            }
+
+            if (res.message) {
+                setMess(res.message)
+                setTimeout(() => {
+                    setMess('')
+                }, 5000)
+                props.img = props.img.filter((item) => item._id !== lightBox._id)
+                setLightBox({ newUrl: "", detail: '', _id: "", show: true })
+            }
+
+
+        } catch (error) {
+            console.log(error);
+            setErr("Hiba: próbáld újra")
+            setTimeout(() => {
+                setErr('')
+            }, 5000)
+        }
+
+    }
+
     return (
         <>
+            {err !== "" &&
+                <div className="fixed top-0 pt-2 bg-white text-red-700 text-2xl text-center">{err}</div>
+            }
+            {mess !== "" &&
+                <div className="fixed top-0 pt-2 bg-white text-green-600 text-2xl text-center">{mess}</div>
+            }
             <section>
                 {props.img.map((item) => <Image alt={item.detail} src={item.newUrl} onClick={() => clickOnImage(item.newUrl, item.detail, item._id, item.show)} />)}
             </section>
@@ -63,7 +105,7 @@ const ImageUpdaetPage = (props: { img: Img[] }) => {
                     <div className="flex justify-center items-center">
                         <Image src={lightBox.newUrl} alt={lightBox.detail} />
                         <div>
-                            <Button className="text-red-700">Törlés</Button>
+                            <Button className="text-red-700" onClick={deleteImg}>Törlés</Button>
                             <div>
                                 <form action={action}>
                                     <Card className="w-full max-w-sm">
@@ -77,21 +119,21 @@ const ImageUpdaetPage = (props: { img: Img[] }) => {
                                             <div className="flex flex-col gap-6">
                                                 <div className="mb-5">
                                                     <Label htmlFor="picture-url" className="mb-2">Kép url-je</Label>
-                                                    <Input id="picture-url" name="newUrl" type="text" disabled={isPending} required value={lightBox.newUrl} onChange={inputChangeHandle}/>
+                                                    <Input id="picture-url" name="newUrl" type="text" disabled={isPending} required value={lightBox.newUrl} onChange={inputChangeHandle} />
                                                 </div>
                                                 <div>
                                                     <Label htmlFor="picture-alt" className="mb-2">Kép leírása</Label>
-                                                    <Input id="picture-alt" name="detail" type="text" disabled={isPending} required value={lightBox.detail} onChange={inputChangeHandle}/>
+                                                    <Input id="picture-alt" name="detail" type="text" disabled={isPending} required value={lightBox.detail} onChange={inputChangeHandle} />
                                                 </div>
 
                                                 <div>
                                                     <Label htmlFor="picture-visibility" className="mb-2">Kép leírása</Label>
-                                                    <Checkbox  id="picture-visibility" name="show" disabled={isPending} required checked={lightBox.show} onChange={() => setLightBox({ ...lightBox, show: !lightBox.show })}/>
+                                                    <Checkbox id="picture-visibility" name="show" disabled={isPending} required checked={lightBox.show} onChange={() => setLightBox({ ...lightBox, show: !lightBox.show })} />
                                                 </div>
 
                                                 <div className="hidden">
                                                     <Label htmlFor="picture-id" className="mb-2">Kép azonosítója</Label>
-                                                    <Input id="picture-id" name="_id" type="text" disabled={isPending} required value={lightBox._id}/>
+                                                    <Input id="picture-id" name="_id" type="text" disabled={isPending} required value={lightBox._id} />
                                                 </div>
 
                                             </div>
