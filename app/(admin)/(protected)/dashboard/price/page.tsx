b2c7372f417ea210ad66f4f8becb67934ce2ef09
Kevin Bencs <kevinbencs8@gmail.com>
2026-04-29 21:37:20 +0200

    Worked on form of /dashboard/price.

--- app/(admin)/(protected)/dashboard/price/page.tsx
+++ app/(admin)/(protected)/dashboard/price/page.tsx
@@ -1,8 +1,11 @@
+import MainElement from '@/app/_components/dashboard/price/mainElement'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='w-full'>
+      <MainElement/>
+    </div>
   )
 }
 
---
+++ app/_components/dashboard/price/mainElement.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { useActionState } from "react"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+const MainElement = () => {
+    return (
+        <div className='flex justify-center'>
+            <Card className="w-full max-w-sm">
+                <CardHeader>
+                    <CardTitle>Új ár hozzáadása</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <form /*action={action}*/   >
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="category">Kategoria</Label>
+                                <Input
+                                    id="category"
+                                    name="category"
+                                    type="text"
+                                    placeholder="Pl: festés"
+                                    required
+                                   /* disabled={isPending}*/
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="name">Név</Label>
+                                </div>
+                                <Input id="name" type="text" name="name" required /*disabled={isPending}*/ />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="price">Ár</Label>
+                                </div>
+                                <Input id="price" name="price" type="number" required /*disabled={isPending}*/ />
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex-col gap-2">
+                    <Button type="submit" className="w-full" /*disabled={isPending}*/>
+                        Mentés
+                    </Button>
+
+                </CardFooter>
+            </Card>
+        </div>
+    )
+}
+
+export default MainElement
