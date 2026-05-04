9084c8652652f0fa8fae1a6b42f840367ea04c15
Kevin Bencs <kevinbencs8@gmail.com>
2026-05-04 21:52:22 +0200

    Worked on /adatvedelem page.

--- app/(publish)/adatvedelem/page.tsx
+++ app/(publish)/adatvedelem/page.tsx
@@ -1,8 +1,120 @@
-import React from 'react'
+
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+      <h1 className="text-3xl mb-10">Adatvédelem</h1>
+      <div className="mb-10">
+
+
+        Az Ön által megadott személyes adatokat kezeli:
+
+        Bencs Kornél György
+
+        Székhely: Budapest 1119, Vahot utca 5. 3/9
+
+        Adószám: 55719193-1-43
+
+        Kérjük, figyelmesen olvassa el jelen adatkezelési tájékoztatónkat (a továbbiakban: Tájékoztató), melyben személyes adatai kezelését érintő gyakorlatunkat ismertetjük az Általános Adatvédelmi Rendelet (a GDPR, azaz az Európai Parlament és Tanács 2016/679. rendelete) szerint. Ez a Tájékoztató az Adatkezelő szolgáltatásait fogyasztóként igénybe vevő személyekre (Ön) vonatkozik. Bemutatja, hogy az Adatkezelő hogyan gyűjti, használja fel és osztja meg bizonyos esetekben harmadik felekkel az Ön személyes adatait, továbbá információval szolgál az Ön adatkezeléssel összefüggő „érintetti” jogairól.
+
+        Az Adatkezelő (Impresszum) elkötelezett e honlapot felkereső egyének személyes adatainak és magánéletének védelme iránt. Jelen tájékoztató és nyilatkozat mindenekelőtt az ügyfelek és hírlevél-feliratkozók személyes adatainak interneten keresztül történő bizalmas kezeléséről és védelméről szól, mindazonáltal ide tartoznak az oldal látogatói is. Összefoglaljuk, hogy a tulajdonos milyen Önre vonatkozó adatokat hogyan gyűjthet és használhat fel, illetve milyen módon használhatják fel. Az adatok illetéktelen személyek részére nem kerülnek átadásra és ezen tájékoztatóban meghatározott formában kerülnek felhasználásra.
+
+        A szerződésekben egyedi esetekben az adatkezelés célja módosulhat, mely az adott szerződésben rögzített feltételekkel történik.
+
+        A tájékoztató fontos információt nyújt Önnek személyes adatai védelméről és ehhez fűződő jogairól. Ha a felhasználó nem fogadja el ezen feltételeket, jogában áll a honlap használatát felfüggeszteni és személyes adat megadása nélkül a böngészést befejezni.
+
+        ADATKEZELŐ FELELŐSSÉGE ÉS ELÉRHETŐSÉGE
+        Adatkezeléssel kapcsolatos információk
+
+        Jelen Tájékoztatóban ismertetett adatkezelések tekintetében Bencs Kornél az adatkezelő (a továbbiakban: Adatkezelő).
+
+        Adatvédelmi eljárásainkat érintő észrevétel, kérdés vagy panasz esetén az alábbi elérhetőségeken vagyunk elérhetőek: Bencs Kornél, Budapest 1119. Etele út 28. 2/12. vagy írjon a bencskornel@budapestiszobafesto.hu e-mail címre.
+
+        Adatkezelő	Felelősségi körök
+        Bencs Kornél György
+        Budapest 1119. Etele út 28. 2/12.	Összes tevékenység
+        Választásának megfelelően Ön személyes adatai megadása nélkül is jogosult hozzáférni weboldalainkhoz, viszont a böngészéshez elengedhetetlen a süti – cookie szabályzat elfogadása, hírlevél-feliratkozás esetén pedig az adatvédelmi tájékoztató és nyilatkozat jóváhagyása. Személyes adat az összes információ, amely közvetett vagy közvetlen módon, azonosító szám szerint vagy az ehhez tartozó adatok alapján azonosítható természetes személyre vonatkozik. Abban az esetben, hogyha az oldal Tulajdonosa az Ön személyes adatait gyűjti, az adatgyűjtés átlátható és bizalmas adatkezeléssel történik.
+
+        Az Ön személyes adatai olyan információk, melyek segítségével az Ön személye beazonosítható, például: vezetéknév, utónév, állampolgárság, telefonszám, postacím, e-mail cím. Az Ön személyes adatait önkéntesen megadott módon, a tulajdonos az oldalon történő böngészéskor gyűjti, megjelölve minden esetben a cél eléréséhez szükségszerűen megadandó adatokat. Ha nem kívánja megadni ezen adatokat, akkor nem férhet hozzá a honlap egyes funkcióihoz, szolgáltatásaihoz. Az opcionálisan megadandó adatok gyűjtése a felhasználói igények megismerését, a honlap és szolgáltatások fejlesztését szolgálja.
+
+        A Tulajdonos minden intézkedést megtesz, hogy az általa kezelt személyes adatok pontosak, naprakészek legyenek.
+
+        Az alábbi személyes adatok kezelése történik:
+
+        Vezetéknév, keresztnév, cégnév és kapcsolattartó neve (cég esetén)
+        Székhely címe (cég esetén), lakcím, levelezési cím
+        E-mail cím
+        Telefonszám
+        Rendszer információk (IP-cím, rendszer verzió, felbontás, statisztika a honlapon megtekintett oldalakról, böngészési szokások, viselkedési mintázat)
+        Adatkezelés időtartama:
+        Direkt marketing célú hozzájárulások tekintetében a felhasználó hozzájárulásának visszavonásáig
+        Profiladatok tekintetében az utolsó bejelentkezéstől számított 4 év
+        Vásárlások adatai tekintetében a Számv. tv. 169. § (2) bekezdése alapján 8 év
+        Adattovábbítás feltételei
+        Az Adatkezelő az általa kezelt adatokat – a szükséges mértékben – továbbíthatja a következő területeken tevékenykedő, általa kijelölt személyek, valamint társaságok részére:
+
+        Adatfeldolgozás
+        Jogi képviselet
+        Jogviták kezelésére jogszabály alapján jogosult szervek
+        Kézbesítés
+        Könyvelés
+        Követeléskezelés
+        Marketing
+        Számlázás
+        Adatfeldolgozást végző vagy adatkezelést érintő cégek, személyek
+        A tulajdonos, Bencs Kornél György végzi az adatok kezelését. A tulajdonoson kívül más cégek is részt vesznek a szerződéses ügyfelek adatainak kezelésében, tárolásában.
+
+        Mely cégek kapcsolódnak az adatkezeléshez?
+        A Tulajdonos tevékenysége során az adatkezelésben érintett cégek:
+
+        Tárhely szolgáltató: Név: Sybell, 1158 Budapest, Késmárk u. 7./b. II. em. 206., telefonszán: +36 1 707 67 26 – adatvédelmi nyilatkozat
+        Google Inc.: 1600 Amphitheatre Parkway, Mountain View, CA 94043, USA, további elérhetőségek, levelező rendszer, felhőben történő adat és fájl tárolás, online dokumentum kezelés, továbbá a hozzátartozó szolgáltatások: Google Drive, Google Docs, Google Search Console, Google Analytics, Google AdSense, Google AdWords, YouTube, Blogger, Chrome böngésző támogatása – Google adatvédelme és adatvédelmi elvei.
+        Facebook Ireland Ltd.: 4 Grand Canal Square, Grand Canal Harbour, Dublin 2 Ireland, a Facebook, Instagram, Messenger és Facebook által kínált egyéb termékek, funkciók támogatása – adatkezelési szabályzat és adatvédelmi tisztviselő elérhetősége.
+        Twitter International Company: One Cumberland Place, Fenian Street, Dublin 2, D02 AX07 IRELAND, a Twitter által kínált termékek, funkciók használata – adatkezelési szabályzat és ászf.
+        Speciális esetekben a szerződés tartalmazhat további adatfeldolgozó személyeket vagy cégeket.
+        Adattulajdonosi jogok és jogérvényesítés
+        Amennyiben nem szeretné személyes adatai kereskedelmi ajánlatok közlésére történő felhasználását, joga van mindezt indoklás nélkül megtiltani. Törlési kérelmét írásban szükséges megküldenie az Adatkezelő részére, e-mailben vagy postai levél formájában, az Impresszumban megjelölt elérhetőségekre.
+
+        Kiskorúak és korlátozottan cselekvőképes személyes adatainak védelme
+        Bencs Kornél internetes weboldala, valamint megjelenéseinek tartalma többnyire nem 13 év alatti kiskorúak részére szól. Nem kezelünk és nem gyűjtünk 13 év alatti gyermekekről személyes adatokat igazolható szülői vagy gondviselői hozzájárulás nélkül. Szülei vagy gondviselői kérhetik a gyermekre vonatkozó adatok kiadását, törlését. Ha az Adatkezelő számára az adatkezelés, adatfeldolgozás folyamán válik nyilvánvalóvá az életkor, abban az esetben a szülői hozzájárulás beszerzése céljából az adatok felhasználhatók.
+
+        Törvényes képviselő hozzájárulása szükséges cselekvőképtelen és korlátozottan cselekvőképes kiskorú személy nyilatkozatához, kivéve a mindennapi életben tömegesen előforduló regisztrációt célzó és különösebb megfontolást nem igénylő szolgáltatásrészeket.
+
+        Cookie-k (sütik) kezelése
+        A Cookie nyilatkozatban olvashat bővebben a sütik (cookie-k) kezeléséről.
+
+        <section>
+          <h2 className="text-2xl mb-4">Fogalmak</h2>
+          <ul>
+            <li className="mb-2"><span className="font-bold">Adatfeldolgozó:</span> természetes vagy jogi személy, közhatalmi szerv, ügynökség vagy bármely egyéb szerv, amely az adatkezelő nevében személyes adatokat kezel</li>
+            <li className="mb-2"><span className="font-bold">Adatkezelés:</span> a személyes adatokon vagy adatállományokon automatizált vagy nem automatizált módon végzett bármely művelet vagy műveletek összessége, például: gyűjtés, rögzítés, rendszerezés, tagolás, tárolás, átalakítás, megváltoztatás, lekérdezés, betekintés, felhasználás, közlés, továbbítás, terjesztés, egyéb módon történő hozzáférhetővé tétel, összehangolás, összekapcsolás, korlátozás, törlés, megsemmisítés</li>
+            <li className="mb-2"><span className="font-bold">Adattovábbítás:</span> az Adatkezelő által kezelt személyes adatok harmadik személyek számára történő hozzáférhetővé tétele</li>
+            <li className="mb-2"><span className="font-bold">Adatvédelmi incidens:</span> a biztonság sérülése, amely a továbbított, tárolt vagy más módon kezelt személyes adatok véletlen vagy jogellenes megsemmisítését, elvesztését, megváltoztatását, jogosulatlan közlését vagy az azokhoz való jogosulatlan hozzáférést eredményezi</li>
+            <li className="mb-2"><span className="font-bold">Azonosítható természetes személy:</span> természetes személy, aki közvetlen vagy közvetett módon, valamely azonosító, például: név, azonosító szám, helymeghatározó adat, online azonosító, fizikai, fiziológiai, genetikai, szellemi, gazdasági, kulturális vagy szociális azonosságára vonatkozó egy vagy több tényező alapján azonosítható</li>
+            <li className="mb-2"><span className="font-bold">Címzett:</span> természetes vagy jogi személy, közhatalmi szerv, ügynökség vagy bármely egyéb szerv, amellyel a személyes adatot közlik, függetlenül attól, hogy harmadik fél-e</li>
+            <li className="mb-2"><span className="font-bold">Érintett hozzájárulása:</span> az érintett akaratának önkéntes, konkrét, megfelelő tájékoztatáson alapuló, egyértelmű kinyilvánítása, amellyel az érintett nyilatkozat vagy a megerősítést félreérthetetlenül kifejező cselekedet útján jelzi, hogy beleegyezését adja az őt érintő személyes adatok kezeléséhez</li>
+            <li className="mb-2"><span className="font-bold">Érintett:</span> bármely információ alapján azonosított vagy azonosítható természetes személy</li>
+            <li className="mb-2"><span className="font-bold">Felhasználók:</span> az Adatkezelő által üzemeltetett, https://budapestiszobafesto.hu weboldalon és az ott meghatározott egyéb címeken elérhető weboldalak látogatói</li>
+            <li className="mb-2"><span className="font-bold">Harmadik fél:</span> természetes vagy jogi személy, közhatalmi szerv, ügynökség vagy bármely egyéb szerv, amely nem azonos az érintettel, az adatkezelővel, az adatfeldolgozóval vagy azokkal a személyekkel, akik az adatkezelő vagy adatfeldolgozó közvetlen irányítása alatt a személyes adatok kezelésére felhatalmazást kaptak</li>
+            <li className="mb-2"><span className="font-bold">Hatóság:</span> Nemzeti Adatvédelmi és Információszabadság Hatóság (székhely: 1125 Budapest, Szilágyi Erzsébet fasor 22/c., telefon: 06-1 391-1400, fax: 06-1 391-1410, e-mail: ugyfelszolgalat@naih.hu, weboldal: naih.hu)</li>
+            <li className="mb-2"><span className="font-bold">Partner/Ügyfél:</span> természetes személy, aki az Adatkezelővel nem weboldalon köt szerződést termék értékesítésére vagy szolgáltatás nyújtására</li>
+            <li className="mb-2"><span className="font-bold">Regisztráció:</span> a weboldal felhasználó személyes adatainak rögzítése, mellyel felhasználói fiókot hoz létre</li>
+            <li className="mb-2"><span className="font-bold">Személyes adat:</span> az érintettre vonatkozó bármely információ</li>
+            <li className="mb-2"><span className="font-bold">Weboldal:</span> az Adatkezelő által üzemeltetett, https://budapestiszobafesto.hu/ és az ott meghatározott egyéb címeken elérhető weboldalai</li>
+          </ul>
+        </section>
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      </div>
+    </>
   )
 }
 
