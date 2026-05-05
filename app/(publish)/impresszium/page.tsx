1e8e2eda9a2f66ca5db5fd27def213dfc12c5cc6
Kevin Bencs <kevinbencs8@gmail.com>
2026-05-05 21:05:27 +0200

    Created structure of /imprusszium page. Must change personal data.

--- app/(publish)/impresszium/page.tsx
+++ app/(publish)/impresszium/page.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <section>
+      <h1 className='text-2xl mb-12'>Impresszium</h1>
+      <ul>
+        <li className='mb-2'><span className='font-bold'>Szolgáltató neve:</span> Bencs Kornél György</li>
+        <li className='mb-2'><span className='font-bold'>Szolgáltató székhelye:</span> Budapest 1119 Vahot utca 5. 3/9.</li>
+        <li className='mb-2'><span className='font-bold'>mail cím:</span>E bencskornel@budapestiszobafesto.hu</li>
+        <li className='mb-2'><span className='font-bold'>Kamarai tagság:</span> Budapesti Kereskedelmi és Iparkamara</li>
+        <li className='mb-2'><span className='font-bold'>Tárhely szolgáltató adatai:</span>   Sybell, 1158 Budapest, Késmárk u. 7./b. II. em. 206., telefonszán: +36 1 707 67 26 - adatvédelmi nyilatkozat</li>
+      </ul>
+    </section>
   )
 }
 
