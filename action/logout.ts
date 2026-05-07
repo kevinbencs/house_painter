f338ac45ce5ebde3c3679193d0556398d31b4ae3
Kevin Bencs <kevinbencs8@gmail.com>
2026-05-07 22:11:11 +0200

    Finished logout server action.

--- action/logout.ts
+++ action/logout.ts
@@ -1,7 +1,14 @@
 "use server"
 
+import { cookies } from "next/headers";
+
 export const logout = async () => {
     try {
+        const cookieStore = await cookies();
+
+        cookieStore.delete("longAuthToken")
+        cookieStore.delete("shortAuthToken")
+
         return {message: "Success"}
     } catch (error) {
         console.error(error);
