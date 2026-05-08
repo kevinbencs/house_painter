7a864ef631d5a07f14f6b3c2bede9d8c7d76d7bf
Kevin Bencs <kevinbencs8@gmail.com>
2026-05-08 23:30:58 +0200

    Added Service mongoose schema

---
+++ models/Service.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const ServiceSchema = new mongoose.Schema({
+    heading: {
+        type: String,
+        require: true
+    },
+    text: {
+        type: String,
+        require: true,
+    },
+    detail: {
+        type: String,
+        require: true
+    },
+    keywords: {
+        type: Array,
+        require: true
+    },
+    image: {
+        type: String,
+        require: true
+    }
+}, {timestamps: true})
+
+
+export default mongoose.models.Service || mongoose.model("Service", ServiceSchema)
