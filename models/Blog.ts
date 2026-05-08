4c11239aa5132613c441b8ad277bfa474d9a14c9
Kevin Bencs <kevinbencs8@gmail.com>
2026-05-08 23:29:20 +0200

    Added Blog mongoosed schema.

---
+++ models/Blog.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const BlogSchema = new mongoose.Schema({
+    heading: {
+        type: String,
+        require: true
+    },
+    text: {
+        type: String,
+        require: true,
+    },
+    detail: {
+        type: String,
+        require: true
+    },
+    keywords: {
+        type: Array,
+        require: true
+    },
+    image: {
+        type: String,
+        require: true
+    }
+}, {timestamps: true})
+
+
+export default mongoose.models.Blog || mongoose.model("Blog", BlogSchema)
