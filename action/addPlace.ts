d70b4e5e4742694757c7975e5b133daf115873b1
Kevin Bencs <kevinbencs8@gmail.com>
2026-05-10 21:21:07 +0200

    Worked on addPlace and addService server action.

--- action/addPlace.ts
+++ action/addPlace.ts
@@ -1,12 +1,50 @@
 "use server"
 
+import { checkAuth } from "@/lib/checkAuth";
+import { handleMongooseError } from "@/lib/mongo";
+import Place from "@/models/Place";
+import { blogServPlaceSchema } from "@/schema/schema";
 import { ActionState } from "@/typeScriptType/form"
 
 export const addPlace = async (_prevState: ActionState, formData: FormData) => {
-    try {
-        return {message: ""}
+     try {
+        const auth = await checkAuth()
+
+        if (auth.error) return { error: "Kérlek jelentkezz be." };
+        
+
+        const heading = formData.get('heading') as string;
+        const text = formData.get('text') as string;
+        const detail = formData.get('detail') as string;
+        const keywords = formData.get('keywords') as string;
+        const image = formData.get('image') as string;
+
+        const res = blogServPlaceSchema.safeParse({
+            heading,
+            text,
+            detail,
+            keywords,
+            image
+        })
+
+        if (res.error) {
+            console.log(res.error.issues);
+            return { failed: res.error.issues.map((item) => item.message) }
+        }
+
+        const blog = new Place({
+            heading,
+            text,
+            detail,
+            image,
+            keywords
+        });
+
+        await blog.save();
+
+        return {message: "Új hely hozzáadva"}
     } catch (error) {
-        console.error(error)
-        return {error: ""}
+        const err = handleMongooseError(error)
+        return {error: err}
     }
 }
--- action/addService.ts
+++ action/addService.ts
@@ -1,12 +1,50 @@
 "use server"
 
+import { checkAuth } from "@/lib/checkAuth";
+import { handleMongooseError } from "@/lib/mongo";
+import Service from "@/models/Service";
+import { blogServPlaceSchema } from "@/schema/schema";
 import { ActionState } from "@/typeScriptType/form"
 
 export const addService= async (_prevState: ActionState, formData: FormData) => {
     try {
-        return {message: ""}
+        const auth = await checkAuth()
+
+        if (auth.error) return { error: "Kérlek jelentkezz be." };
+        
+
+        const heading = formData.get('heading') as string;
+        const text = formData.get('text') as string;
+        const detail = formData.get('detail') as string;
+        const keywords = formData.get('keywords') as string;
+        const image = formData.get('image') as string;
+
+        const res = blogServPlaceSchema.safeParse({
+            heading,
+            text,
+            detail,
+            keywords,
+            image
+        })
+
+        if (res.error) {
+            console.log(res.error.issues);
+            return { failed: res.error.issues.map((item) => item.message) }
+        }
+
+        const blog = new Service({
+            heading,
+            text,
+            detail,
+            image,
+            keywords
+        });
+
+        await blog.save();
+
+        return {message: "Új hely hozzáadva"}
     } catch (error) {
-        console.error(error)
-        return {error: ""}
+        const err = handleMongooseError(error)
+        return {error: err}
     }
 }
