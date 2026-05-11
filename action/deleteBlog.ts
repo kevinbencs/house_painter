7c433324e4f8a4a821e004b482dd5c7161dc9ead
Kevin Bencs <kevinbencs8@gmail.com>
2026-05-11 20:10:44 +0200

    Worked on deleteBlog server action.

--- action/deleteBlog.ts
+++ action/deleteBlog.ts
@@ -1,15 +1,25 @@
 "use server"
 
+import { checkAuth } from "@/lib/checkAuth";
 import { handleMongooseError } from "@/lib/mongo";
+import Blog from "@/models/Blog";
 import { deleteSchema } from "@/schema/schema";
 
 export const deletePlace = async (_id: string) => {
     try {
+        const authRes = await checkAuth();
+
+        if(authRes.error) return { error: "Kérlek jelentkezz be." };
+
         const res = deleteSchema.safeParse(_id);
         if (res.error?.issues) {
             console.log(res.error.issues)
             return { failed: res.error.issues.map((item) => item.message) }
         }
+
+        await Blog.findByIdAndDelete(_id)
+
+        return {message: "Blog törölve."}
 
     } catch (error) {
         const Error = await handleMongooseError(error);
