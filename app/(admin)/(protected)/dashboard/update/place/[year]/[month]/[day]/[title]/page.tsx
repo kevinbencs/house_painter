f801d0b908978a1c47e90b9f84a1e87dbcc40c3c
Kevin Bencs <kevinbencs8@gmail.com>
2026-05-28 23:05:35 +0200

    Data selection on two update pages (/dashboad/udpate/place/[year].../[title] and /dashboard/update/service/.../[title])

--- app/(admin)/(protected)/dashboard/update/place/[year]/[month]/[day]/[title]/page.tsx
+++ app/(admin)/(protected)/dashboard/update/place/[year]/[month]/[day]/[title]/page.tsx
@@ -3,28 +3,38 @@ import DynamicPagesForm from "@/app/_components/dashboard/dynamicPagesForm"
 import { addPlace } from "@/action/addPlace"
 import { checkAuth } from "@/lib/checkAuth";
 import { redirect } from "next/navigation";
+import Place from "@/models/Place";
+import { connection } from "next/server";
 
 const page = async ({ params }: { params: Promise<{ year: string, month: string, day: string, title: string }> }) => {
   const auth = await checkAuth()
 
   if (auth.error) redirect('/');
 
+  await connection()
+  const par = await params
+  const title = par.title.replaceAll('-', ' ')
+  const data = await Place.findOne({heading: title})
+
+  
+
   const res = {
     error: undefined,
     failed: undefined,
     data: {
-      title: "",
-      text: "",
-      cover_img_id: "",
-      keyword: [],
-      id: "",
-      detail: "",
+      title: data.heading,
+      text: data.text,
+      cover_img_id: data.image,
+      keyword: data.keywords.split(";"),
+      id: String(data._id),
+      detail: data.detail,
     }
   }
 
+
   return (
     <div className="w-full">
-      <DynamicPagesForm params={await params} res={res} serverAction={addPlace} />
+      <DynamicPagesForm  res={res} serverAction={addPlace} />
     </div>
   )
 }
--- app/(admin)/(protected)/dashboard/update/service/[year]/[month]/[day]/[title]/page.tsx
+++ app/(admin)/(protected)/dashboard/update/service/[year]/[month]/[day]/[title]/page.tsx
@@ -2,6 +2,8 @@ import DynamicPagesForm from "@/app/_components/dashboard/dynamicPagesForm"
 import { addService } from "@/action/addService"
 import { checkAuth } from "@/lib/checkAuth";
 import { redirect } from "next/navigation";
+import { connection } from "next/server";
+import Service from "@/models/Service";
 
 const page = async ({ params }: { params: Promise<{ year: string, month: string, day: string, title: string }> }) => {
 
@@ -9,23 +11,30 @@ const page = async ({ params }: { params: Promise<{ year: string, month: string,
 
   if (auth.error) redirect('/');
 
+  await connection()
+  const par = await params
+  const title = par.title.replaceAll('-', ' ')
+  const data = await Service.findOne({heading: title})
+
+  
 
   const res = {
     error: undefined,
     failed: undefined,
     data: {
-      title: "",
-      text: "",
-      cover_img_id: "",
-      keyword: [],
-      id: "",
-      detail: "",
+      title: data.heading,
+      text: data.text,
+      cover_img_id: data.image,
+      keyword: data.keywords.split(";"),
+      id: String(data._id),
+      detail: data.detail,
     }
   }
 
+
   return (
     <div className="w-full">
-      <DynamicPagesForm params={await params} res={res} serverAction={addService} />
+      <DynamicPagesForm  res={res} serverAction={addService} />
     </div>
   )
 }
