2d0f5d416adc0f9b0f460383461fc3d3f3264c47
Kevin Bencs <kevinbencs8@gmail.com>
2026-05-29 23:10:42 +0200

    Added display function to /dashboard/updat/.....

--- app/_components/dashboard/updatePagesList.tsx
+++ app/_components/dashboard/updatePagesList.tsx
@@ -33,13 +33,33 @@ const UpdatePagesList = (props: { list: BSPClientList[], page: string, }) => {
     }
   }
 
+  const displayItem = async (id: string) => {
+    switch (props.page) {
+      case "blog":
+        const res = await displayBlog(id);
+        if (res.error) alert(res.error);
+        if (res.failed) alert(res.failed.join(" "));
+        break;
+      case "service":
+        const resSer = await displayService(id);
+        if (resSer.error) alert(resSer.error);
+        if (resSer.failed) alert(resSer.failed.join(" "));
+        break;
+      case "place":
+        const resPla = await dispalyPlace(id);
+        if (resPla.error) alert(resPla.error);
+        if (resPla.failed) alert(resPla.failed.join(" "));
+        break;
+    }
+  }
+
 
   return (
     <section>
       {props.list.map((item) => <li className="flex justify-between w-full mb-2 border-b-2" key={item.id}>
         <Link href={`/dashboard/update/${props.page}/${item.year}/${item.month}/${item.day}/${item.title.toLowerCase().replaceAll(' ', '-')}`} className="w-full" >{item.title}</Link>
         {item.hide === false && < button onClick={() => deleteItem(item.id)} className="p-2 text-red-600"><MdDelete /></button>}
-        {item.hide === true && < button onClick={() => deleteItem(item.id)} className="p-2 text-green-600"><FaPlusCircle /></button>}
+        {item.hide === true && < button onClick={() => displayItem(item.id)} className="p-2 text-green-600"><FaPlusCircle /></button>}
       </li>)
       }
     </section >
