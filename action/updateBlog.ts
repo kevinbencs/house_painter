e6a5c2c9976794c1c2b4f74a42586b2a95d055a3
Kevin Bencs <kevinbencs8@gmail.com>
2026-06-07 15:52:59 +0200

    Added text checking to updateBlog server action.

--- action/updateBlog.ts
+++ action/updateBlog.ts
@@ -1,9 +1,10 @@
 "use server"
 
 import { checkAuth } from "@/lib/checkAuth";
+import { chooseTypeOfTextItem } from "@/lib/checkTextBSP";
 import { handleMongooseError } from "@/lib/mongo";
 import Blog from "@/models/Blog";
-import {  blogServPlaceSchemaId } from "@/schema/schema";
+import { blogServPlaceSchemaId } from "@/schema/schema";
 import { ActionState } from "@/typeScriptType/form";
 
 
@@ -13,12 +14,12 @@ export const updateImage = async (_prevState: ActionState, formData: FormData) =
 
         if (authRes.error) return { error: "Kérlek jelentkezz be." };
 
-        const heading = (formData.get('heading') as string).replaceAll('\n','');
-        const text = (formData.get('text') as string).replaceAll('\n','');
-        const detail = (formData.get('detail') as string).replaceAll('\n','');
-        const keywords = (formData.get('keywords') as string).replaceAll('\n','');
-        const image = (formData.get('image') as string).replaceAll('\n','');
-        const _id = (formData.get('_id') as string).replaceAll('\n','');
+        const heading = (formData.get('heading') as string).replaceAll('\n', '');
+        const text = (formData.get('text') as string).replaceAll('\n', '');
+        const detail = (formData.get('detail') as string).replaceAll('\n', '');
+        const keywords = (formData.get('keywords') as string).replaceAll('\n', '');
+        const image = (formData.get('image') as string).replaceAll('\n', '');
+        const _id = (formData.get('_id') as string).replaceAll('\n', '');
 
         const res = blogServPlaceSchemaId.safeParse({
             heading,
@@ -32,6 +33,12 @@ export const updateImage = async (_prevState: ActionState, formData: FormData) =
         if (res.error?.issues) {
             console.log(res.error.issues)
             return { failed: res.error.issues.map((item) => item.message) }
+        }
+
+        const textArr = text.split("\n")
+        for (let i = 0; i < textArr.length; i++) {
+            const mess = chooseTypeOfTextItem(textArr[i])
+            if (mess.indexOf('Error') > -1) return { error: mess }
         }
 
         await Blog.findByIdAndUpdate(_id, {
