e2ad1f870a94da832853495a3101cd7da055c531
Kevin Bencs <kevinbencs8@gmail.com>
2026-06-10 22:28:02 +0200

    Worked on /szolgaltatasok/[heading] (metadata, generateStaticParams)

--- app/(publish)/(mainPages)/szolgaltatas/[services]/page.tsx
+++ app/(publish)/(mainPages)/szolgaltatas/[services]/page.tsx
@@ -1,6 +1,82 @@
-import React from 'react'
+import { BSPRender } from "@/typeScriptType/blogServPlace"
+import { notFound } from "next/navigation"
+import type { Metadata, ResolvingMetadata } from 'next';
+import { Img } from '@/typeScriptType/img';
+import { connectToMongo } from "@/lib/mongo";
+import Service from "@/models/Service";
+import { getServiceByHeading } from "@/lib/data";
+import Image from "@/models/Image"
 
-const page = () => {
+export async function generateMetadata(
+  { params }: { params: Promise<{ heading: string }> },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+
+  const { heading } = await params
+
+
+  const data: BSPRender | null = await getServiceByHeading(decodeURIComponent(heading))
+
+  if (data === null) return {}
+
+  const imgData: (Img | null) = await Image.findById(data.image)
+
+
+
+  return {
+    title: decodeURIComponent(heading.replaceAll('-', ' ')),
+    keywords: data.keywords.split(';'),
+    description: data.detail,
+    openGraph: {
+      locale: 'hu_HU',
+      title: decodeURIComponent(heading.replaceAll('-', ' ')),
+      description: data.detail,
+      type: 'website',
+      url: `${process.env.URL}/blog/${heading}`,
+      images: [
+        {
+          url: process.env.URL + '/api/images' + imgData?.newUrl,
+          alt: imgData?.detail
+        }
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: decodeURIComponent(heading.replaceAll('-', ' ')),
+      description: data.detail,
+      images: [
+        {
+          url: process.env.URL + '/api/images' + imgData?.newUrl,
+          alt: imgData?.detail
+        }
+      ],
+    },
+
+  }
+}
+
+export async function generateStaticParams() {
+  await connectToMongo()
+
+  const data = await Service.find({}, { heading: 1 })
+
+  if (data.length === 0) return ([{ heading: '__placeholder__' }])
+  return data.map((item) => ({ heading: item.heading }))
+}
+
+
+
+
+const page = async ({ params }: { params: Promise<{ heading: string }> }) => {
+
+  const { heading } = await params;
+
+  if (heading === '__placeholder__') notFound()
+
+
+  const data: BSPRender | null = await getServiceByHeading(decodeURIComponent(heading))
+
+  if (data === null) notFound();
   return (
     <div>page</div>
   )
