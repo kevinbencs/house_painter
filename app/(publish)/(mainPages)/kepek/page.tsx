432f2cc09ca26a27efd337216d53e8b97f5d5df6
Kevin Bencs <kevinbencs8@gmail.com>
2026-06-11 22:43:12 +0200

    Worked on metadata of /kepek pages.

--- app/(publish)/(mainPages)/kepek/page.tsx
+++ app/(publish)/(mainPages)/kepek/page.tsx
@@ -1,7 +1,29 @@
 import ImagePage from '@/app/_components/image/imageContent'
 import Pagination from '@/app/_components/image/pagination'
 import { getNumbOfImag, getTwentyImg } from '@/lib/data'
+import { Metadata } from 'next'
 import { cacheLife, cacheTag } from 'next/cache'
+
+
+export const metadata: Metadata = {
+  title: 'Képek',
+  description: 'Képek a szobafestésről, tapétázásról és egyéb felújításai munkálatokról.',
+  keywords: ['szobafestés', 'tapétázás', "szobafestés képek", "tapétázás képek"],
+  openGraph: {
+    siteName: 'Budafestő',
+    locale: 'hu_HU',
+    type: 'website',
+    images: [{ url: "/api/images", alt: 'Budafestő - Képek' }],
+  },
+  twitter: {
+      card: 'summary_large_image',
+      title: "Képek",
+      description: 'Képek a szobafestésről, tapétázásról és egyéb felújításai munkálatokról.',
+      images: [{ url: "/api/images", alt: 'Budafestő - Képek' }],
+    },
+
+}
+
 
 
 const Page = async () => { 
