9088c0770ac5be36bfea7c646de7ce2bf03e0180
Kevin Bencs <kevinbencs8@gmail.com>
2026-06-12 23:23:42 +0200

    Worked on metadata of /kepek/[header] pages.

--- app/(publish)/(mainPages)/kepek/[page]/page.tsx
+++ app/(publish)/(mainPages)/kepek/[page]/page.tsx
@@ -1,5 +1,28 @@
 import Main from '@/app/_components/image/main'
+import { Metadata } from 'next'
 import { Suspense } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Képek',
+  description: 'Képek a szobafestésről, tapétázásról és egyéb felújításai munkálatokról.',
+  keywords: ['szobafestés', 'tapétázás', "szobafestés képek", "tapétázás képek"],
+  openGraph: {
+    siteName: 'Budafestő',
+    locale: 'hu_HU',
+    type: 'website',
+    images: [{ url: "/api/images", alt: 'Budafestő - Képek' }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Képek",
+    description: 'Képek a szobafestésről, tapétázásról és egyéb felújításai munkálatokról.',
+    images: [{ url: "/api/images", alt: 'Budafestő - Képek' }],
+  },
+
+}
+
+
+
 
 const Page = async ({ params }: { params: Promise<{ page: string }> }) => {
 
