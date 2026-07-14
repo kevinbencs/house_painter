4b9d767d1a468684aa954f039158e43de88c149c
Kevin Bencs <kevinbencs8@gmail.com>
2026-07-14 22:39:54 +0200

    Fixed the robot.ts (missing route).

--- app/robot.ts
+++ app/robot.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/dashboard/', '/login/','/forgotpassword/'],
+      disallow: ['/dashboard/*', '/login/*','/forgotpassword/', '/new2fa'],
     },
     sitemap: 'https://acme.com/sitemap.xml',
   }
