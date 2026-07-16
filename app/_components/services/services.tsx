e521cbaeaeefeab6ab9099071570d2fb5aa431a0
Kevin Bencs <kevinbencs8@gmail.com>
2026-07-16 23:52:28 +0200

    Created services component for place pages.

---
+++ app/_components/services/services.tsx
@@ -0,0 +1,34 @@
+
+
+const Services = () => {
+    return (
+        <section >
+            <h2 className="text-2xl">Szolgáltatásaim</h2>
+            <ul className="flex flex-wrap">
+                <li>
+                    <h3 className="">Lakásfestés</h3>
+                    <p>Teljes lakások, egyedi helyiségek vagy csak egy-egy fal újrafestése. Vállalok tisztasági festést is.</p>
+                </li>
+                <li>
+                    <h3>Homlokzatfestés</h3>
+                    <p>Társasházak és családi házak homlokzatának felújítása, mely különösen fontos, ahol az időjárásnak jobban kitett felületekről van szó.</p>
+                </li>
+                <li>
+                    <h3>Tapétázás</h3>
+                    <p>Klasszikus és design tapéták felhelyezése, régi tapéta eltávolítása, aljzat-előkészítés.</p>
+                </li>
+                <li>
+                    <h3>Glettelés és gipszkartonozás</h3>
+                    <p>Egyenetlen falak, repedések javítása.</p>
+                </li>
+                <li>
+                    <h3>Festés utáni takarítás</h3>
+                    <p>Igény esetén elvégezem a bútorok letakarását, védelmét, illetve a munka befejezése után a takarítást is, hogy azonnal birtokba vehesse a megújult teret.</p>
+                </li>
+            </ul>
+        </section>
+    )
+}
+
+
+export default Services
