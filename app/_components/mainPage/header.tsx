942014ffb25a1a9e014080f9f5c59e0121b15829
Kevin Bencs <kevinbencs8@gmail.com>
2026-08-01 23:58:55 +0200

    Added section to header on home page.

--- app/_components/mainPage/header.tsx
+++ app/_components/mainPage/header.tsx
@@ -36,46 +36,45 @@ const Header = () => {
 
 
   return (
-    <section>
-      <div>
-        <div className="md: relative md:h-screen md:w-full md:m-0 mt-auto mb-auto"
-          
+    <>
+      <div className="md: relative md:h-screen md:w-full md:m-0 mt-auto mb-auto"
+
+      >
+        <div className={`flex  md:absolute md:top-0 md:left-0`}
         >
-          <div className={`flex  md:absolute md:top-0 md:left-0`}
-          >
 
-            <div className={`md:w-screen md:h-screen -z-20 bg-fixed bg-cover bg-center bg-no-repeat shrink-0 min-w-screen md:min-w-auto min-h-200 md:min-h-auto`}
-              key={cards[0].id}
-              style={{
-                backgroundImage: `url(${cards[0].image})`,
-              }}
-            />
+          <div className={`md:w-screen md:h-screen -z-20 bg-fixed bg-cover bg-center bg-no-repeat shrink-0 min-w-screen md:min-w-auto min-h-200 md:min-h-auto`}
+            key={cards[0].id}
+            style={{
+              backgroundImage: `url(${cards[0].image})`,
+            }}
+          />
 
-            <div className={`md:w-screen md:h-screen -z-10 fixed top-0 left-0 bg-fixed bg-cover bg-center bg-no-repeat shrink-0 min-w-screen md:min-w-auto min-h-200 md:min-h-auto`}
-              key={cards[1].id}
-              style={{ 
-                backgroundImage: `url(${cards[1].image})`,
-                clipPath: animate
-                  ? "inset(0 0% 0 0)"      
-                  : "inset(0 100% 0 0)",  
-                transition: "clip-path 0.6s ease",
-              }}
-            />
+          <div className={`md:w-screen md:h-screen -z-10 fixed top-0 left-0 bg-fixed bg-cover bg-center bg-no-repeat shrink-0 min-w-screen md:min-w-auto min-h-200 md:min-h-auto`}
+            key={cards[1].id}
+            style={{
+              backgroundImage: `url(${cards[1].image})`,
+              clipPath: animate
+                ? "inset(0 0% 0 0)"
+                : "inset(0 100% 0 0)",
+              transition: "clip-path 0.6s ease",
+            }}
+          />
 
-          </div>
-
-          <h1 className="text-6xl text-center left-0 w-screen text-shadow-lg text-shadow-gray-700 text-white top-10 absolute"
-          >
-            Szobafestés, mázolás Budapesten és környékén
-          </h1>
         </div>
+
+
+      </div>
+      <section>
+        <h1 className="text-6xl text-center left-0 w-screen text-shadow-lg text-shadow-gray-700 text-white top-10 absolute">
+          Szobafestés, mázolás Budapesten és környékén
+        </h1>
         <p className="text-3xl md:text-6xl text-center left-0 w-screen absolute top-[70%] text-white text-shadow-lg text-shadow-gray-700">
           Budapest teljes területén minden munka FELMÉRÉSE és az ÁRAJÁNLAT KÉSZÍTÉSE <span className="text-red-700">DÍJTALAN!</span>
         </p>
-        
-      </div>
+      </section>
+    </>
 
-    </section>
   )
 }
 
