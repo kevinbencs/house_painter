832c2fc8208cd17c74981c0ab28c430474d43553
Kevin Bencs <kevinbencs8@gmail.com>
2026-09-13 23:42:11 +0200

    Places on footer. [ski ci]

--- app/_components/footer/places.tsx
+++ app/_components/footer/places.tsx
@@ -36,19 +36,19 @@ const Places = async () => {
     try {
         data = await getPlaceFooter()
     } catch (error) {
-        
+
 
         return (
             <div></div>
         )
     }
-    
-    const name = data.map(item => item.heading.slice(12, item.heading.indexOf('-')-12))
+
+    const name = data.map(item => item.heading.slice(12, item.heading.indexOf('-') - 12))
 
 
     const city = place.map((item) => {
-        if(name.indexOf(item) > -1)  {let p = data[name.indexOf(item)]; return <Link href={'/helyek/'+p.heading.slice(0, p.heading.indexOf('.') + 9).replaceAll(' ', '-')} className="hover:underline" key={`footer-place-${p._id}`}>{p.heading.slice(12, p.heading.indexOf('-')-12)}</Link>}
-        return  <li  className="hover:underline" key={`footer-place-${item+'-key'}`}>{item}</li>
+        if (name.indexOf(item) > -1) { let p = data[name.indexOf(item)]; return <Link href={'/helyek/' + p.heading.slice(0, p.heading.indexOf('.') + 9).replaceAll(' ', '-')} className="hover:underline" key={`footer-place-${p._id}`}>{p.heading.slice(12, p.heading.indexOf('-') - 12)}</Link> }
+        return <li className="hover:underline" key={`footer-place-${item + '-key'}`}>{item}</li>
     })
 
     return (
@@ -58,7 +58,7 @@ const Places = async () => {
                 {city.slice(0, city.length / 2)}
             </section>
 
-            
+
             <section className="flex flex-col items-center">
                 {city.slice(city.length / 2, city.length)}
             </section>
